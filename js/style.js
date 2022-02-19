ca20b4eab33892156c025c90da2f21a324948948
Eduard <Rampage854@gmail.com>
2022-02-19 12:19:24 +0300

    Init commit

---
+++ js/style.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('.head_burger').click(function (event) {
+		
+	});
+});
+
+.head_burger.onclick = function myFunction() {
+	$('.head_burger, .header_nav').toggleClass('active');
